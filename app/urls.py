dbb21a99602d0a72a10b842eb347efcdbc5ae8ea
lucasfpac <lucasfortunato1@gmail.com>
2024-09-12 18:10:11 +0100

    urls update

--- app/urls.py
+++ app/urls.py
@@ -1,19 +1,3 @@
-"""
-URL configuration for app project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
@@ -21,6 +5,7 @@ from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
+# Configurações do Swagger
 schema_view = get_schema_view(
     openapi.Info(
         title="Dash Points",
@@ -36,16 +21,16 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('users.urls')), 
-    path('api/', include('points.urls')), 
-    path('api/', include('customers.urls')),
+    path('dashpoints/api/', include('users.urls')), 
+    path('dashpoints/api/', include('points.urls')), 
+    path('dashpoints/api/', include('customers.urls')),
 
-    # JWT Authentication URLs
-    path('api/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('api/validate-token/', TokenVerifyView.as_view(), name='token_verify'),
+    # URLs de autenticação JWT
+    path('dashpoints/api/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('dashpoints/api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('dashpoints/api/validate-token/', TokenVerifyView.as_view(), name='token_verify'),
 
-    #Swagger
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    # Documentação Swagger
+    path('dashpoints/swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('dashpoints/redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
