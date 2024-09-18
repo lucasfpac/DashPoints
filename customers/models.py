9b607e3243cd721496225130ae3183c182bc0ddc
lucasfpac <lucasfortunato1@gmail.com>
2024-09-18 20:31:10 +0100

    added selected_store on Customer

--- customers/models.py
+++ customers/models.py
@@ -3,13 +3,21 @@ from phonenumber_field.modelfields import PhoneNumberField
 
 class Customer(models.Model):
     full_name = models.CharField(max_length=100)
-    cpf_cnpj = models.CharField(unique= True ,max_length=18, blank=True, null=True) 
+    cpf_cnpj = models.CharField(unique=True, max_length=18, blank=True, null=True)
     email = models.EmailField(max_length=255, blank=True, null=True)
     phone = PhoneNumberField(blank=True, null=True)
     cep = models.CharField(max_length=9, default='00000-000')
     city = models.CharField(max_length=50, blank=True, null=True)
-    uf = models.CharField(max_length=2) 
-    missingstore = models.CharField(max_length=100, blank=True, null=True) 
+    uf = models.CharField(max_length=2)
+    missingstore = models.CharField(max_length=100, blank=True, null=True)
+    selected_store = models.ForeignKey('Store', on_delete=models.SET_NULL, null=True, blank=True)
 
     def __str__(self):
         return f"{self.full_name}"
+
+class Store(models.Model):
+    name = models.CharField(max_length=255)
+    address = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
