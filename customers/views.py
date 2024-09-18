659e5e6c8f18d607361e6de3081d6b1080a00728
lucasfpac <lucasfortunato1@gmail.com>
2024-09-18 20:10:14 +0100

    cpf/cnpj url search update

--- customers/views.py
+++ customers/views.py
@@ -1,20 +1,21 @@
-from rest_framework.permissions import AllowAny #remove after
+from rest_framework.permissions import AllowAny  # remove after
 from rest_framework import viewsets
-from .models import Customer
 from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework import status
-from .serializers import CustomerSerializer
 from rest_framework import generics
+from .models import Customer
+from .serializers import CustomerSerializer
 
 class CustomerViewSet(viewsets.ModelViewSet):
     queryset = Customer.objects.all()
     serializer_class = CustomerSerializer
+    permission_classes = [AllowAny]
 
-    @action(detail=False, methods=['get'])
+    @action(detail=False, methods=['get'], url_path='cpf_cnpj')
     def get_by_cpf_cnpj(self, request):
         cpf_cnpj = request.query_params.get('cpf_cnpj', None)
-        if cpf_cnpj is not None:
+        if cpf_cnpj:
             try:
                 customer = Customer.objects.get(cpf_cnpj=cpf_cnpj)
                 serializer = self.get_serializer(customer)
@@ -22,9 +23,9 @@ class CustomerViewSet(viewsets.ModelViewSet):
             except Customer.DoesNotExist:
                 return Response({'error': 'Customer not found'}, status=status.HTTP_404_NOT_FOUND)
         return Response({'error': 'CPF/CNPJ is required'}, status=status.HTTP_400_BAD_REQUEST)
-    permission_classes = [AllowAny] 
-    
+
 
 class CustomerCreateView(generics.CreateAPIView):
     queryset = Customer.objects.all()
     serializer_class = CustomerSerializer
+    permission_classes = [AllowAny]
