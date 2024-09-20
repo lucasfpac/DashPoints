67d32e1bda705bbc4f0e1e74b1d898a13fbb4dcd
lucasfpac <lucasfortunato1@gmail.com>
2024-09-21 00:11:17 +0100

    cpf get update

--- customers/serializers.py
+++ customers/serializers.py
@@ -1,7 +1,18 @@
 from rest_framework import serializers
 from .models import Customer
+from points.models import Purchases
+
+class PurchasesSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Purchases
+        fields = ['id'] 
 
 class CustomerSerializer(serializers.ModelSerializer):
+    purchases = serializers.SerializerMethodField()
+
     class Meta:
         model = Customer
-        fields = '__all__'
+        fields = ['id', 'full_name', 'cpf_cnpj', 'email', 'phone', 'cep', 'city', 'uf', 'missingstore', 'selected_store', 'purchases']
+
+    def get_purchases(self, obj):
+        return Purchases.objects.filter(customer=obj).values_list('id', flat=True)
--- customers/views.py
+++ customers/views.py
@@ -1,28 +1,22 @@
 from rest_framework.permissions import AllowAny  # remove after
-from rest_framework import viewsets
-from rest_framework.decorators import action
-from rest_framework.response import Response
-from rest_framework import status
 from rest_framework import generics
+from rest_framework import viewsets
+from rest_framework.response import Response
 from .models import Customer
 from .serializers import CustomerSerializer
 
 class CustomerViewSet(viewsets.ModelViewSet):
     queryset = Customer.objects.all()
     serializer_class = CustomerSerializer
-    permission_classes = [AllowAny]
 
-    @action(detail=False, methods=['get'], url_path='cpf_cnpj')
-    def get_by_cpf_cnpj(self, request):
-        cpf_cnpj = request.query_params.get('cpf_cnpj', None)
-        if cpf_cnpj:
-            try:
-                customer = Customer.objects.get(cpf_cnpj=cpf_cnpj)
-                serializer = self.get_serializer(customer)
-                return Response(serializer.data, status=status.HTTP_200_OK)
-            except Customer.DoesNotExist:
-                return Response({'error': 'Customer not found'}, status=status.HTTP_404_NOT_FOUND)
-        return Response({'error': 'CPF/CNPJ is required'}, status=status.HTTP_400_BAD_REQUEST)
+    def retrieve_by_cpf_cnpj(self, request, cpf_cnpj=None):
+        try:
+            customer = Customer.objects.get(cpf_cnpj=cpf_cnpj)
+            serializer = CustomerSerializer(customer)
+            return Response(serializer.data)
+        except Customer.DoesNotExist:
+            return Response({"detail": "Customer not found."}, status=404)
+    permission_classes = [AllowAny]
 
 
 class CustomerCreateView(generics.CreateAPIView):
