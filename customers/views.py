d9ac5ff8445c85fcd7719c66524e43099c9b5ad2
lucasfpac <lucasfortunato1@gmail.com>
2024-09-21 00:22:58 +0100

    cpf/cnpj fix

--- customers/views.py
+++ customers/views.py
@@ -4,6 +4,8 @@ from rest_framework import viewsets
 from rest_framework.response import Response
 from .models import Customer
 from .serializers import CustomerSerializer
+from rest_framework import status
+
 
 class CustomerViewSet(viewsets.ModelViewSet):
     queryset = Customer.objects.all()
@@ -11,11 +13,12 @@ class CustomerViewSet(viewsets.ModelViewSet):
 
     def retrieve_by_cpf_cnpj(self, request, cpf_cnpj=None):
         try:
-            customer = Customer.objects.get(cpf_cnpj=cpf_cnpj)
+            # Verifica o cliente pelo CPF/CNPJ exato (sem espaços ou formatações extras)
+            customer = Customer.objects.get(cpf_cnpj=cpf_cnpj.strip())
             serializer = CustomerSerializer(customer)
-            return Response(serializer.data)
+            return Response(serializer.data, status=status.HTTP_200_OK)
         except Customer.DoesNotExist:
-            return Response({"detail": "Customer not found."}, status=404)
+            return Response({"detail": "Customer not found."}, status=status.HTTP_404_NOT_FOUND)
     permission_classes = [AllowAny]
 
 
