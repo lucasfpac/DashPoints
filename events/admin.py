7c62e6bc8721e44a20a969d265aeae29588c0acc
lucasfpac <lucasfortunato1@gmail.com>
2024-09-25 10:42:51 +0100

    event admin

--- events/admin.py
+++ events/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from events.models import Events
 
-# Register your models here.
+class EventAdmin(admin.ModelAdmin):
+    list_display = ('name', 'start_date', 'end_date', 'voucher_value')
+    search_fields = ('name', 'start_date', 'end_date', 'voucher_value')
+admin.site.register(Events, EventAdmin)
