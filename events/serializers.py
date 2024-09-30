daf1dc3da7252210aa4a5a9a329caa879e69b10e
lucasfpac <lucasfortunato1@gmail.com>
2024-09-30 13:03:49 +0100

    admin and serializer events update

--- events/serializers.py
+++ events/serializers.py
@@ -4,7 +4,7 @@ from .models import Events
 class EventSerializer(serializers.ModelSerializer):
     class Meta:
         model = Events
-        fields = ['name', 'start_date', 'end_date', 'voucher_value']
+        fields = ['id', 'name', 'start_date', 'end_date', 'voucher_value']
 
 
 
