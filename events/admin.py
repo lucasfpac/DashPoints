b705aede77b17020b567f46a4d27257d032f1dfe
lucasfpac <lucasfortunato1@gmail.com>
2024-09-30 13:03:49 +0100

    admin and serializer events update

--- events/admin.py
+++ events/admin.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from events.models import Events
 
 class EventAdmin(admin.ModelAdmin):
-    list_display = ('name', 'start_date', 'end_date', 'voucher_value')
-    search_fields = ('name', 'start_date', 'end_date', 'voucher_value')
+    list_display = ('id', 'name', 'start_date', 'end_date', 'voucher_value')
+    search_fields = ('id', 'name', 'start_date', 'end_date', 'voucher_value')
 admin.site.register(Events, EventAdmin)
